ab6b652fe01145a6888baf6501abd68d7512d597
Dale McDiarmid <dalem@elastic.co>
2016-09-14 14:11:05 +0100

    Test scripts for automation

---
+++ run_test.sh
@@ -0,0 +1,29 @@
+if [ -z "$1" ]; then
+echo "Specify watch name e.g. run_test.sh <foldername>"
+fi
+num_tests=0
+pass=0
+fails=0
+echo "--------------------------------------------------"
+for test in `ls $1/tests/*.json`; do
+echo "Running test $test"
+python run_test.py --test_file $test
+if [ $? -eq 0 ]; then
+let pass=pass+1
+else
+let fails=fails+1
+fi
+let num_tests=num_tests+1
+echo "--------------------------------------------------"
+done;
+
+echo "$num_tests tests run: $pass passed. $fails failed."
+if [ $fails -eq 0 ]; then
+exit 0
+else
+exit 1
+fi
+
+
+
+
