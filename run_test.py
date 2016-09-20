14b91cecfc8ebc38a59fb108990cec811c474f40
Dale McDiarmid <dalem@elastic.co>
2016-09-20 15:24:09 +0100

    Virtual Env + support for basic auth + Use xpack lib

--- run_test.py
+++ run_test.py
@@ -3,19 +3,21 @@ import sys
 __author__ = 'dalem@elastic.co'
 
 import datetime
-from elasticsearch_watcher import WatcherClient
 from elasticsearch import Elasticsearch
+from elasticsearch_xpack import XPackClient
 import argparse
 import json
 
 parser = argparse.ArgumentParser(description='Index Connection Log data into ES with the last event at the current time')
 parser.add_argument('--host',help='host name')
 parser.add_argument('--port',help='port')
+parser.add_argument('--user',help='user')
+parser.add_argument('--password',help='password')
 parser.add_argument('--test_file',help='test file')
 
-parser.set_defaults(host='localhost',port="9200",test_file='data.json')
+parser.set_defaults(host='localhost',port="9200",test_file='data.json',user='elastic',password='changeme')
 args = parser.parse_args()
-es = Elasticsearch([args.host+":"+args.port])
+es = Elasticsearch([args.host+":"+args.port],http_auth=(args.user, args.password))
 
 def find_item(list, key):
     for item in list:
@@ -31,20 +33,25 @@ with open(args.test_file,'r') as test_file:
     except:
         pass
     with open(test['mapping_file'],'r') as mapping_file:
-        body=json.loads(mapping_file.read())
-        es.indices.create(index=test["index"],body=body)
+        es.indices.create(index=test["index"],body=json.loads(mapping_file.read()))
+    #Load pipeline if its declared
+    params={}
+    if "ingest_pipeline_file" in test:
+        with open(test['ingest_pipeline_file'],'r') as ingest_pipeline_file:
+            es.index(index="_ingest",doc_type="pipeline",id=test["watch_name"],body=json.loads(ingest_pipeline_file.read()))
+            params["pipeline"]=test["watch_name"]
     #Index data
     current_data=last_time=datetime.datetime.utcnow()
     i=0
     for event in test['events']:
         #All offsets in seconds
         event_time=current_data+datetime.timedelta(seconds=int(event['offset'] if 'offset' in event else 0))
-        event["@timestamp"]=event_time.strftime('%Y-%m-%dT%H:%M:%S.%f%z') if not '@timestamp' in event else event["@timestamp"]
-        es.index(index=test['index'],doc_type=test['type'],body=event,id=event['id'] if "id" in event else i)
+        event["@timestamp"]=event_time.strftime('%Y-%m-%dT%H:%M:%S.%fZ') if not '@timestamp' in event else event["@timestamp"]
+        es.index(index=test['index'],doc_type=test['type'],body=event,id=event['id'] if "id" in event else i,params=params)
         i+=1
     es.indices.refresh(index=test["index"])
     #Load Watch and Execute
-    watcher = WatcherClient(es)
+    watcher = XPackClient(es).watcher
     with open(test['watch_file'],'r') as watch_file:
         watch=json.loads(watch_file.read())
         watcher.put_watch(id=test["watch_name"],body=watch)
@@ -52,6 +59,10 @@ with open(args.test_file,'r') as test_file:
         #Confirm Matches
         match = test['match'] if 'match' in test else True
         print("Expected: Watch Condition: %s"%match)
+        if not 'condition' in response['watch_record']['result']:
+            print("Condition not evaluated due to watch error")
+            print("TEST FAIL")
+            sys.exit(1)
         met=response['watch_record']['result']['condition']['met']
         print("Received: Watch Condition: %s"%met)
         if match:
